5a3257be6d3a6d33c24c77e41b1b7fa2dc1c5dfc
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-07-27 10:01:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 impprt telebot
 
-bot=telebot.TeleBot("")
+bot=telebot.TeleBot("1867389893:AAEhOoduV-aOecDnJ5mZ-WlMcwu_YfqxA94")
 
 @bot.massege_handler(commands=["start"])
 def send_welcome(massege):
